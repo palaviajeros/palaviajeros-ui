6e74091be529c812afda762c8a9fbd3dd9b432a3
Andrei de la Cruz <andrei.delacruz@gmail.com>
2024-06-23 18:22:11 +0200

    Chore: Extracted travel packages html to its own component

--- src/app/lib/travelPackages.ts
+++ src/app/lib/travelPackages.ts
@@ -1,30 +1,4 @@
-export interface TravelPackageDto {
-    countryCode: string,
-    countryName: string,
-    packageName: string,
-    packageId: number,
-    inclusions: Services[],
-    exclusions: Services[],
-    dates: DateRange[],
-    itinerary: DayPlan[],
-    imageUrls: string[],
-}
-
-export interface DayPlan {
-    dayNo: number,
-    activities: string[]
-}
-
-export interface DateRange {
-    startDate: Date,
-    endDate: Date,
-}
-
-export enum Services {
-    FLIGHT,
-    HOTEL,
-    MEALS,
-}
+import {Services, TravelPackageDto} from "@/app/shared/models/travelPackageDto";
 
 const getImages = (fetchedPackages: TravelPackageDto): string[] => {
     const baseFolder = './public';
