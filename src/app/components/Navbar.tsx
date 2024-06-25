c833f7af60017ea1fc88f7855964514cdb050cdb
LeaHaelin <lea8haelin@gmail.com>
2024-06-26 00:00:10 +0200

    add home layout

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -1,15 +1,53 @@
+"use client";
+
 import React from "react";
 import Link from "next/link";
+import Image from "next/image";
+import { useState } from "react";
+import { Container, Group, Burger, Menu } from "@mantine/core";
+import { useDisclosure } from "@mantine/hooks";
+import classes from "@/app/components/Navbar.module.scss";
+
+const links = [
+  { link: "/", label: "Home" },
+  { link: "/travel-packages", label: "Packages" },
+  { link: "/about", label: "About" },
+  { link: "/contact", label: "Contact" },
+];
 
 const Navbar: React.FC = () => {
+  const [opened, { toggle }] = useDisclosure(false);
+  const [active, setActive] = useState(links[0].link);
+
+  const items = links.map((link) => (
+    <Link
+      key={link.label}
+      href={link.link}
+      className={opened ? classes.link : classes.toggle}
+      data-active={active === link.link || undefined}
+      onClick={(event) => {
+        event.preventDefault();
+        setActive(link.link);
+      }}
+    >
+      {link.label}
+    </Link>
+  ));
+
   return (
-    <nav>
-      <ul>
-        <Link href="/">Home</Link>
-        <Link href="/travel-packages">Packages</Link>
-        <Link href="/">About</Link>
-        <Link href="/">Contact</Link>
-      </ul>
+    <nav className={classes.header}>
+      <Container size="md" className={classes.inner}>
+        <Image src="/palaviajeros_logo.png" alt="" width={200} height={100} />
+        <Group gap={5} visibleFrom="xs">
+          {items}
+        </Group>
+        <div className={classes.nav_mb}>
+          <Burger opened={opened} onClick={toggle} hiddenFrom="xs" size="sm" />
+          <div className={`classes.burger ${opened ? undefined : "none"}`}>
+            {items}
+          </div>
+        </div>
+      </Container>
     </nav>
   );
 };
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,30 @@
+import { Title, Text, Image, Container, Flex } from "@mantine/core";
+import classes from "@/app/Home.module.scss";
+
 export default function Home() {
-  return <h1>Home</h1>;
+  const textContents = {
+    title: "Buckle up!",
+    text: "Prepare for an adventure like never before! The Palaviajeros team is preparing amazing itineraries for you. For now, fasten your seatbelts as we taxi through the runway. ;)",
+  };
+
+  return (
+    <Container className={classes.container}>
+      <Flex>
+        <div className={classes.contents}>
+          <Title className={classes.title} order={1}>
+            {textContents.title}
+          </Title>
+          <Text className={classes.text}>{textContents.text}</Text>
+        </div>
+        <Image
+          className={classes.image}
+          radius="md"
+          h={600}
+          w="auto"
+          fit="contain"
+          src="/ex-home-image.jpg"
+        />
+      </Flex>
+    </Container>
+  );
 }
