dfdb61f8f22e52d9035c19161bc731ee1c3eb759
Sam Chu <samchupiano@gmail.com>
2024-06-26 01:14:39 +0200

    Revert changes to travelPackages.ts

--- src/app/lib/travelPackages.ts
+++ src/app/lib/travelPackages.ts
@@ -1,44 +1,33 @@
-import {
-  Services,
-  TravelCountryPackages,
-  TravelPackageDto
-} from "@/app/shared/models/travelPackageDto";
+import {Services, TravelCountryPackages, TravelPackageDto} from "@/app/shared/models/travelPackageDto";
 
 import fs from "fs";
 
-const baseFolder = "./public";
-const getImages = (
-  fetchedPackages: TravelPackageDto,
-  country: TravelCountryPackages
-): string[] => {
-  let result: string[] = [];
-  const imagePath = `/packages/${
-    country.countryCode
-  }/${fetchedPackages.packageId.toLowerCase()}/`;
+const baseFolder = './public';
+const getImages = (fetchedPackages: TravelPackageDto, country: TravelCountryPackages): string[] => {
 
-  try {
-    let files: string[] = fs.readdirSync(`${baseFolder}${imagePath}`);
-    files.forEach((file) => {
-      result.push(`${imagePath}${file}`);
-    });
-    return result;
-  } catch (err) {
-    // skip non existent directories
-  }
-  return [];
+    let result: string[] = [];
+    const imagePath = `/packages/${country.countryCode}/${fetchedPackages.packageId.toLowerCase()}/`;
+
+    try {
+        let files: string[] = fs.readdirSync(`${baseFolder}${imagePath}`);
+        files.forEach(file => {
+            result.push(`${imagePath}${file}`)
+        });
+        return result;
+    } catch (err) {
+        // skip non existent directories
+    }
+    return [];
 };
 
 const getTravelPackages = (): TravelCountryPackages[] => {
-  const data = fs.readFileSync("./public/packages/travelpackages.json", {
-    encoding: "utf8"
-  });
-  let packagesByCountry: TravelCountryPackages[] = JSON.parse(data);
+    const data = fs.readFileSync('./public/packages/travelpackages.json', {encoding: 'utf8'});
+    let packagesByCountry: TravelCountryPackages[] = JSON.parse(data);
 
-  packagesByCountry.forEach((country) =>
-    country.packages.forEach((p) => (p.imageUrls = getImages(p, country)))
-  );
+    packagesByCountry.forEach(country =>
+        country.packages.forEach(p => p.imageUrls = getImages(p, country)));
 
-  return packagesByCountry;
-};
+    return packagesByCountry;
+}
 
 export default getTravelPackages;
