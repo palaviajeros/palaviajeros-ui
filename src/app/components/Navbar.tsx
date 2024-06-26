0d63778375deccaaffda983c8b47f7a8652c3291
Lea Haelin Kim <87936540+LeaHaelin@users.noreply.github.com>
2024-06-26 15:23:03 +0200

    14 home layout with placeholder using mantine (#26)
    * add home layout
    
    * refactor styles and navbar
    
    * refactor navbar
    
    * refactor home page layout

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -1,15 +1,59 @@
+"use client";
+
 import React from "react";
 import Link from "next/link";
+import Image from "next/image";
+import { useState } from "react";
+import { Container, Group, Burger, Menu } from "@mantine/core";
+import { useDisclosure } from "@mantine/hooks";
+import classes from "@/app/components/Navbar.module.scss";
+
+const links = [
+  { link: "/", label: "Home" },
+  { link: "/travel-packages", label: "Packages" },
+  { link: "/about", label: "About" },
+  { link: "/contact", label: "Contact" },
+];
 
 const Navbar: React.FC = () => {
+  const [opened, { toggle }] = useDisclosure(false);
+  const [active, setActive] = useState(links[0].link);
+
+  const items = links.map((link) => (
+    <Link
+      key={link.label}
+      href={link.link}
+      className={classes.link}
+      data-active={active === link.link || undefined}
+      onClick={(event) => {
+        setActive(link.link);
+      }}
+    >
+      {link.label}
+    </Link>
+  ));
+
   return (
-    <nav>
-      <ul>
-        <Link href="/">Home</Link>
-        <Link href="/travel-packages">Packages</Link>
-        <Link href="/">About</Link>
-        <Link href="/">Contact</Link>
-      </ul>
+    <nav className={classes.header}>
+      <Container size="md" className={classes.inner}>
+        <Image src="/palaviajeros_logo.png" alt="" width={200} height={100} />
+        <Group gap={5} visibleFrom="xs">
+          {items}
+        </Group>
+        <Menu>
+          <Menu.Target>
+            <Burger
+              opened={opened}
+              onClick={toggle}
+              hiddenFrom="xs"
+              size="sm"
+            />
+          </Menu.Target>
+          <Menu.Dropdown className={classes.dropdown}>
+            <Menu.Item>{items}</Menu.Item>
+          </Menu.Dropdown>
+        </Menu>
+      </Container>
     </nav>
   );
 };
