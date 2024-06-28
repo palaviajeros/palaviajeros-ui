3642e8e9db618da3576a43375852df69c4d2d039
LeaHaelin <lea8haelin@gmail.com>
2024-06-28 19:19:16 +0200

    refactor burger nav

--- src/app/components/Navbar/Navbar.tsx
+++ src/app/components/Navbar/Navbar.tsx
@@ -1,23 +1,36 @@
 "use client";
 
 import React from "react";
+import { useState, useEffect } from "react";
 import Link from "next/link";
 import Image from "next/image";
-import { useState } from "react";
-import { Container, Group, Burger, Menu } from "@mantine/core";
-import { useDisclosure } from "@mantine/hooks";
+import { Container, Group, Burger, Drawer } from "@mantine/core";
+import { useDisclosure, useMediaQuery } from "@mantine/hooks";
+import { IconX } from "@tabler/icons-react";
 import classes from "@/app/components/Navbar/Navbar.module.scss";
 
 const links = [
   { link: "/", label: "Home" },
   { link: "/travel-packages", label: "Packages" },
   { link: "/about", label: "About" },
-  { link: "/contact", label: "Contact" },
+  // { link: "/contact", label: "Contact" },
 ];
 
 const Navbar: React.FC = () => {
-  const [opened, { toggle }] = useDisclosure(false);
   const [active, setActive] = useState(links[0].link);
+  const [opened, { toggle, close }] = useDisclosure(false);
+  const isLargerThan500px = useMediaQuery("(min-width: 500px)"); //to close Drawer-nav on over 500px screen size
+
+  useEffect(() => {
+    if (isLargerThan500px) {
+      close();
+    }
+  }, [isLargerThan500px, close]);
+
+  const handleLinkClick = (link: string) => {
+    setActive(link);
+    close(); // Close the drawer
+  };
 
   const items = links.map((link) => (
     <Link
@@ -27,6 +40,7 @@ const Navbar: React.FC = () => {
       data-active={active === link.link || undefined}
       onClick={(event) => {
         setActive(link.link);
+        handleLinkClick(link.link);
       }}
     >
       {link.label}
@@ -40,19 +54,21 @@ const Navbar: React.FC = () => {
         <Group gap={5} visibleFrom="xs">
           {items}
         </Group>
-        <Menu>
-          <Menu.Target>
-            <Burger
-              opened={opened}
-              onClick={toggle}
-              hiddenFrom="xs"
-              size="sm"
-            />
-          </Menu.Target>
-          <Menu.Dropdown className={classes.dropdown}>
-            <Menu.Item>{items}</Menu.Item>
-          </Menu.Dropdown>
-        </Menu>
+        <>
+          <Burger size="md" opened={opened} onClick={toggle} hiddenFrom="xs" />
+          <Drawer
+            opened={opened}
+            onClose={close}
+            title=""
+            padding="md"
+            size={200}
+            position="right"
+            overlayProps={{ backgroundOpacity: 0.5, blur: 4 }}
+            closeButtonProps={{ icon: <IconX size={40} stroke={1.5} /> }}
+          >
+            {items}
+          </Drawer>
+        </>
       </Container>
     </nav>
   );
