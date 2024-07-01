9f2ddbef82ca867bda0a8ec2d4d3bd5165224f89
Sam Chu <samchupiano@gmail.com>
2024-07-01 20:25:38 +0200

    Add Header component and postcss config file to process rem/em functions from components imported from Mantine

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,30 +1,9 @@
-import { Title, Text, Image, Container, Flex } from "@mantine/core";
-import classes from "@/app/Home.module.scss";
+import Header from "./components/Home/Header";
 
 export default function Home() {
-  const textContents = {
-    title: "Buckle up!",
-    text: "Prepare for an adventure like never before! The Palaviajeros team is preparing amazing itineraries for you. For now, fasten your seatbelts as we taxi through the runway. ;)",
-  };
-
   return (
-    <Container className={classes.container}>
-      <Flex wrap="wrap" gap="lg">
-        <div className={classes.contents}>
-          <Title className={classes.title} order={1}>
-            {textContents.title}
-          </Title>
-          <Text className={classes.text}>{textContents.text}</Text>
-        </div>
-        <Image
-          className={classes.image}
-          radius="md"
-          h={500}
-          w="auto"
-          fit="contain"
-          src="/samples/ex-home-image.jpg"
-        />
-      </Flex>
-    </Container>
+    <>
+      <Header />
+    </>
   );
 }
