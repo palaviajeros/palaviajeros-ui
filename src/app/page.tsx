853571342938bfbc2a74ca66f1ca95d6f7a148c0
Sam Chu <samchupiano@gmail.com>
2024-07-02 20:04:24 +0200

    Add Bestsellers component and date-fns package

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 import Services from "./components/Home/Services/Services";
 import Header from "./components/Home/Header/Header";
+import Bestsellers from "./components/Home/Bestsellers/Bestsellers";
 
 export default function Home() {
   return (
     <>
       <Header />
+      <Bestsellers />
       <Services />
     </>
   );
