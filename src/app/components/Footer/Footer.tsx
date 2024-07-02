efce9edc7f2ed3fff01fed125cc2ff5c74cf7467
Andrei de la Cruz <andrei.delacruz@gmail.com>
2024-07-02 12:30:56 +0200

    #42 restyled navbar to use primary colors, added linkout to the logo, refactored to nextjs x mantine image, added mantine css, added primary color override

---
+++ src/app/components/Footer/Footer.tsx
@@ -0,0 +1,44 @@
+import React from "react";
+import {Container, Group, ActionIcon, rem} from "@mantine/core";
+import {
+    IconBrandTwitter,
+    IconBrandYoutube,
+    IconBrandInstagram,
+} from "@tabler/icons-react";
+import Image from "next/image";
+import classes from "@/app/components/Footer/Footer.module.scss";
+
+export default function Footer() {
+    return (
+        <footer className={classes.footer}>
+            <Container className={classes.inner}>
+                <Image src="/palaviajeros_logo.png" alt="" width={200} height={100}/>
+                <Group
+                    gap={0}
+                    className={classes.links}
+                    justify="flex-end"
+                    wrap="nowrap"
+                >
+                    <ActionIcon size="lg" color="gray" variant="subtle">
+                        <IconBrandTwitter
+                            style={{width: 18, height: 18}}
+                            stroke={1.5}
+                        />
+                    </ActionIcon>
+                    <ActionIcon size="lg" color="gray" variant="subtle">
+                        <IconBrandYoutube
+                            style={{width: 18, height: 18}}
+                            stroke={1.5}
+                        />
+                    </ActionIcon>
+                    <ActionIcon size="lg" color="gray" variant="subtle">
+                        <IconBrandInstagram
+                            style={{width: 18, height: 18}}
+                            stroke={1.5}
+                        />
+                    </ActionIcon>
+                </Group>
+            </Container>
+        </footer>
+    );
+}
