028c26d1ec0eacee3f9e586f93ff35d2c200fce6
Andrei de la Cruz <andrei.delacruz@gmail.com>
2024-07-03 20:22:25 +0200

    #52 Fixed navbar text size

--- src/app/components/Navbar/Navbar.tsx
+++ src/app/components/Navbar/Navbar.tsx
@@ -2,76 +2,77 @@
 
 import Link from "next/link";
 import NextImage from "next/image";
-import { Flex, Image as MantineImage } from "@mantine/core";
-import { Group, Burger, Drawer } from "@mantine/core";
-import { useDisclosure } from "@mantine/hooks";
+import {Flex, Image as MantineImage} from "@mantine/core";
+import {Group, Burger, Drawer} from "@mantine/core";
+import {useDisclosure} from "@mantine/hooks";
 import classes from "@/app/components/Navbar/Navbar.module.scss";
 
 const links = [
-  { link: "/", label: "Home" },
-  { link: "/travel-packages", label: "Packages" },
-  { link: "/about", label: "About" },
+    {link: "/", label: "Home"},
+    {link: "/travel-packages", label: "Packages"},
+    {link: "/about", label: "About"},
 ];
 
 const Navbar: React.FC = () => {
-  const [opened, { toggle, close }] = useDisclosure(false);
+    const [opened, {toggle, close}] = useDisclosure(false);
 
-  const items = links.map((link) => (
-    <Link
-      key={link.label}
-      href={link.link}
-      className={classes.link}
-      onClick={close}
-    >
-      {link.label}
-    </Link>
-  ));
+    const items = links.map((link) => (
+        <Link
+            key={link.label}
+            href={link.link}
+            className={classes.link}
+            onClick={close}
+        >
+            {link.label}
+        </Link>
+    ));
 
-  return (
-    <nav className={classes.header}>
-      <Flex
-        className={classes.inner}
-        justify={{
-          sm: "space-between",
-          xs: "center",
-        }}
-      >
-        <a href={"/"}>
-          <MantineImage
-            component={NextImage}
-            src="/palaviajeros_logo.png"
-            alt=""
-            width={250}
-            height={40}
-          />
-        </a>
-        <Group gap={5} visibleFrom="sm">
-          {items}
-        </Group>
-        <Burger
-          opened={opened}
-          onClick={toggle}
-          hiddenFrom="sm"
-          size="sm"
-          className={classes.burgerbutton}
-        />
-        <>
-          <Drawer
-            opened={opened}
-            onClose={toggle}
-            position="top"
-            // offset={50}
-            overlayProps={{ backgroundOpacity: 0.2 }}
-            ta="center"
-            size="25%"
-            hiddenFrom="sm"
-          >
-            {items}
-          </Drawer>
-        </>
-      </Flex>
-    </nav>
-  );
+    return (
+        <nav className={classes.header}>
+            <Flex
+                className={classes.inner}
+                justify={{
+                    sm: "space-between",
+                    xs: "center",
+                }}
+            >
+                <a href={"/"}>
+                    <MantineImage
+                        component={NextImage}
+                        src="/palaviajeros_logo.png"
+                        alt=""
+                        width={250}
+                        height={40}
+                    />
+                </a>
+                <Group gap={5} visibleFrom="sm" className={classes.navBar}>
+                    {items}
+                </Group>
+                <Burger
+                    opened={opened}
+                    onClick={toggle}
+                    hiddenFrom="sm"
+                    size="sm"
+                    className={classes.burgerbutton}
+                />
+                <>
+                    <Drawer
+                        opened={opened}
+                        onClose={toggle}
+                        position="top"
+                        // offset={50}
+                        overlayProps={{backgroundOpacity: 0.2}}
+                        ta="center"
+                        size="25%"
+                        hiddenFrom="sm"
+                        className={classes.burgerMenu}
+                    >
+                        {items}
+                    </Drawer>
+                </>
+            </Flex>
+        </nav>
+    );
 };
 
 export default Navbar;
