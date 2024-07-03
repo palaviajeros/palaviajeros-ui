5c3fecf4905b775e67f6ba507694ec739f474ef6
Andrei de la Cruz <andrei.delacruz@gmail.com>
2024-07-03 20:02:10 +0200

    #52 Added subscribe section in the homepage, changed and cleaned some of the styles for About Us and footer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,16 @@
 import Services from "./components/Home/Services/Services";
 import Header from "./components/Home/Header/Header";
 import Bestsellers from "./components/Home/Bestsellers/Bestsellers";
-import {Stack} from "@mantine/core";
+import {Flex, Stack} from "@mantine/core";
+import Subscribe from "@/app/components/Home/Subscribe/Subscribe";
 
 export default function Home() {
     return (
-        <Stack gap={"xl"}>
+        <Flex gap={"xl"} direction={"column"} justify={"space-evenly"}>
             <Header/>
             <Bestsellers/>
             <Services/>
-        </Stack>
+            <Subscribe/>
+        </Flex>
     );
 }
