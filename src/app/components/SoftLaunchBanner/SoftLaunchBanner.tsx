bd1ce73c5122a4d374cee30e0a6c1e5a08fee210
Sam Chu <samchupiano@gmail.com>
2024-07-09 14:21:14 +0200

    Add soft launch notification banner

---
+++ src/app/components/SoftLaunchBanner/SoftLaunchBanner.tsx
@@ -0,0 +1,10 @@
+import classes from "./SoftLaunchBanner.module.css";
+
+export default function SoftLaunchBanner() {
+  return (
+    <div className={classes.soft_launch_wrapper}>
+      🎉 Welcome to the soft launch of Palaviajeros — discover your next
+      adventure with us!
+    </div>
+  );
+}
