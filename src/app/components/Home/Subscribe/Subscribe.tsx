16871d2a59a135a513bd71fe49c214bf3b6f0257
Sam Chu <samchupiano@gmail.com>
2024-07-09 14:39:55 +0200

    Add placeholder to subscribe input component

--- src/app/components/Home/Subscribe/Subscribe.tsx
+++ src/app/components/Home/Subscribe/Subscribe.tsx
@@ -1,40 +1,51 @@
-import {Button, Container, Flex, Group, Text, TextInput, Title} from "@mantine/core";
+import {
+  Button,
+  Container,
+  Flex,
+  Group,
+  Text,
+  TextInput,
+  Title
+} from "@mantine/core";
 import classes from "./Subscribe.module.scss";
 import React from "react";
 
-
 const Subscribe = () => {
-    return (
-        <Flex
-            direction={"column"}
-            mt={"xl"}
-            justify={"center"} className={classes.container}
-            gap={"xl"}
-        >
-            <Title
-                ta={"center"}
-                className={classes.title}
-            >
-                Subscribe to get updates from your fellow Palaviajeros!
-            </Title>
-            <Group justify="center" mt="md">
-                <TextInput
-                    size="xl"
-                    radius="md"
-                    inputSize="35" visibleFrom={"xl"}>
-
-                </TextInput>
-                <TextInput
-                    size="md"
-                    radius="md"
-                    inputSize="35" hiddenFrom={"xl"}>
-
-                </TextInput>
-                <Button size="xl" radius="md" visibleFrom={"xl"}>Contact Us</Button>
-                <Button size="md" radius="md" hiddenFrom={"xl"}>Contact Us</Button>
-            </Group>
-        </Flex>
-    );
-}
+  return (
+    <Flex
+      direction={"column"}
+      mt={"xl"}
+      justify={"center"}
+      className={classes.container}
+      gap={"xl"}
+    >
+      <Title ta={"center"} className={classes.title}>
+        Subscribe to get updates from your fellow Palaviajeros!
+      </Title>
+      <Group justify="center" mt="md">
+        <TextInput
+          size="xl"
+          radius="md"
+          inputSize="35"
+          visibleFrom={"xl"}
+          placeholder="Your email address"
+        ></TextInput>
+        <TextInput
+          size="md"
+          radius="md"
+          inputSize="35"
+          hiddenFrom={"xl"}
+          placeholder="Your email address"
+        ></TextInput>
+        <Button size="xl" radius="md" visibleFrom={"xl"}>
+          Contact Us
+        </Button>
+        <Button size="md" radius="md" hiddenFrom={"xl"}>
+          Contact Us
+        </Button>
+      </Group>
+    </Flex>
+  );
+};
 
 export default Subscribe;
