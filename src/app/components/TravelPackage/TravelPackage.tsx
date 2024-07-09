dc4b4347abd5d074adb20c01b68055aba75eba97
Lea Haelin Kim <87936540+LeaHaelin@users.noreply.github.com>
2024-07-09 17:49:46 +0200

    refactor travelpackages style (#65)

--- src/app/components/TravelPackage/TravelPackage.tsx
+++ src/app/components/TravelPackage/TravelPackage.tsx
@@ -1,121 +1,197 @@
 import React from "react";
 import Image from "next/image";
 import {
-    TravelCountryPackages,
-    DateRange, Services
+  TravelCountryPackages,
+  DateRange,
+  Services,
 } from "@/app/shared/models/travelPackageDto";
-import {Carousel} from "@mantine/carousel";
-import {Flex, Text, Button, Badge, NumberFormatter, rem} from "@mantine/core";
+import { Carousel } from "@mantine/carousel";
 import {
-    IconCheck, IconCircleArrowLeftFilled, IconCircleArrowRightFilled,
-    IconX
+  Flex,
+  Button,
+  Badge,
+  NumberFormatter,
+  rem,
+  Title,
+  Text,
+  Center,
+} from "@mantine/core";
+import {
+  IconCheck,
+  IconCircleArrowLeftFilled,
+  IconCircleArrowRightFilled,
+  IconX,
 } from "@tabler/icons-react";
 import "./TravelPackage.scss";
 import InquiryModalButton from "@/app/components/InquiryModal/InquiryModal";
-import {formatDateRange, getValueOfEnumService} from "@/app/util/Helpers";
+import { formatDateRange, getValueOfEnumService } from "@/app/util/Helpers";
+import { relative } from "path";
 
 interface TravelPackageProps {
-    travelPackage: TravelCountryPackages;
+  travelPackage: TravelCountryPackages;
 }
 
-const TravelPackage = ({travelPackage}: TravelPackageProps) => {
-    return (
-        <>
-            {travelPackage.packages.map((t, index) => {
-                return (
-                    <React.Fragment key={t.code}>
-                        <Flex
-                            gap={{lg: "xl"}}
-                            mb={"xl"}
-                            mt={"xl"}
-                            direction={{
-                                base: "column",
-                                lg: index % 2 === 0 ? "row" : "row-reverse"
-                            }}
-                        >
-                            <div className="carousel-container" style={{flex: 1}}>
-                                <Carousel slideSize="100%" slideGap="lg" align="center" loop height="100%"
-                                          nextControlIcon={<IconCircleArrowRightFilled
-                                              style={{
-                                                  width: rem(24),
-                                                  height: rem(24),
-                                                  color: "white"
-                                              }}/>}
-                                          previousControlIcon={<IconCircleArrowLeftFilled
-                                              style={{
-                                                  width: rem(24),
-                                                  height: rem(24),
-                                                  color: "white"
-                                              }}/>}>
-                                    {t.imageUrls.map((img, index) => {
-                                        return (
-                                            <Carousel.Slide key={index}>
-                                                <Image
-                                                    className="package-photo"
-                                                    layout="responsive"
-                                                    width={16}
-                                                    height={9}
-                                                    src={img}
-                                                    alt="image"
-                                                />
-                                            </Carousel.Slide>
-                                        );
-                                    })}
-                                </Carousel>
-                            </div>
-                            <div style={{flex: 1}}>
-                                <h3>{t.name}</h3>
-                                <Flex gap={"sm"} wrap="wrap">
-                                    {t.travelDates.map((d, index) => {
-                                            return <Badge key={index} color={"red"}>{formatDateRange(d)}</Badge>;
-                                        }
-                                    )}
-                                </Flex>
-                                <div>
-                                    <Flex gap={"lg"} mt={"md"}>
-                                        <Flex direction="column">
-                                            {t.inclusions.map((inclusion, index) => {
-                                                return (
-                                                    <Flex key={index} gap={"sm"}>
-                                                        <IconCheck stroke={3}/>
-                                                        <Text>{`${getValueOfEnumService(inclusion)}`}</Text>
-                                                    </Flex>
-                                                );
-                                            })}
-                                        </Flex>
-                                        <Flex direction="column">
-                                            {t.exclusions.map((exclusion, index) => {
-                                                return (
-                                                    <Flex key={index} gap={"sm"}>
-                                                        <IconX stroke={3}/>
-                                                        <Text>{`${(getValueOfEnumService(exclusion))}`}</Text>
-                                                    </Flex>
-                                                );
-                                            })}
-                                        </Flex>
-                                    </Flex>
-                                    {t.description.map((description) => {
-                                        return (
-                                            <p key={`description-${index}`}>
-                                                {description}
-                                            </p>
-                                        );
-                                    })}
-                                    <Flex direction="row" align="center" gap={"sm"}>
-                                        <InquiryModalButton variant={"primary"} travelPackage={t}>
-                                            Get a Quote
-                                        </InquiryModalButton>
-                                        <NumberFormatter prefix="$ " value={t.price} thousandSeparator
-                                                         style={{fontWeight: "bold"}}/>
-                                    </Flex>
-                                </div>
-                            </div>
-                        </Flex>
-                    </React.Fragment>
-                );
-            })}
-        </>
-    );
+const TravelPackage = ({ travelPackage }: TravelPackageProps) => {
+  return (
+    <>
+      {travelPackage.packages.map((t, index) => {
+        return (
+          <React.Fragment key={t.code}>
+            <Flex
+              gap={{ lg: "xl" }}
+              mb={"xl"}
+              mt={"xl"}
+              direction={{
+                base: "column",
+                lg: "row",
+              }}
+            >
+              <div className="carousel-container" style={{ flex: 1 }}>
+                <Carousel
+                  slideSize="100%"
+                  slideGap="lg"
+                  align="center"
+                  loop
+                  height="100%"
+                  nextControlIcon={
+                    <IconCircleArrowRightFilled
+                      style={{
+                        width: rem(24),
+                        height: rem(24),
+                        color: "white",
+                      }}
+                    />
+                  }
+                  previousControlIcon={
+                    <IconCircleArrowLeftFilled
+                      style={{
+                        width: rem(24),
+                        height: rem(24),
+                        color: "white",
+                      }}
+                    />
+                  }
+                >
+                  {t.imageUrls.map((img, index) => {
+                    return (
+                      <Carousel.Slide key={index}>
+                        <Image
+                          className="package-photo"
+                          layout="responsive"
+                          width={14}
+                          height={9}
+                          src={img}
+                          alt="image"
+                        />
+                      </Carousel.Slide>
+                    );
+                  })}
+                </Carousel>
+              </div>
+              <div style={{ flex: 1 }}>
+                <Title order={3} mb={20} ta={{ base: "center", lg: "left" }}>
+                  {t.name}
+                </Title>
+                <Flex
+                  gap={"sm"}
+                  wrap="wrap"
+                  justify={{ base: "center", lg: "start" }}
+                >
+                  {t.travelDates.map((d, index) => {
+                    return (
+                      <Badge
+                        key={index}
+                        radius="md"
+                        variant="light"
+                        color="cyan"
+                      >
+                        {formatDateRange(d)}
+                      </Badge>
+                    );
+                  })}
+                </Flex>
+                {t.description.map((description) => {
+                  return (
+                    <Text
+                      key={`description-${index}`}
+                      mt="md"
+                      p="md"
+                      fz="h4"
+                      bg="var(--mantine-color-gray-0)"
+                      style={{
+                        borderRadius: "var(--mantine-radius-md)",
+                        boxShadow: "0 0 10px 5px var(--mantine-color-gray-0)",
+                      }}
+                    >
+                      {description}
+                    </Text>
+                  );
+                })}
+                <div>
+                  <Flex
+                    direction={{ base: "row", lg: "column" }}
+                    gap={{ base: "lg", lg: "xs" }}
+                    mt={"md"}
+                    mb={"lg"}
+                    wrap="wrap"
+                  >
+                    <Flex direction="row" gap={"lg"}>
+                      {t.inclusions.map((inclusion, index) => {
+                        return (
+                          <Flex key={index} align="center" gap={"xs"}>
+                            <IconCheck
+                              stroke={3}
+                              color="var(--mantine-color-teal-4)"
+                            />
+                            <Text fz="var(--mantine-font-size-sm)">{`${getValueOfEnumService(
+                              inclusion
+                            )}`}</Text>
+                          </Flex>
+                        );
+                      })}
+                    </Flex>
+                    <Flex direction="row" gap={"lg"}>
+                      {t.exclusions.map((exclusion, index) => {
+                        return (
+                          <Flex key={index} gap={"xs"}>
+                            <IconX
+                              stroke={2}
+                              color="var(--mantine-color-dark-2)"
+                            />
+                            <Text
+                              fz="var(--mantine-font-size-sm)"
+                              color="var(--mantine-color-dark-2)"
+                            >{`${getValueOfEnumService(exclusion)}`}</Text>
+                          </Flex>
+                        );
+                      })}
+                    </Flex>
+                  </Flex>
+                  <Flex
+                    mt={{ base: "xs", lg: "xl" }}
+                    direction="row"
+                    justify="flex-end"
+                    align="center"
+                    gap={{ base: "sm", lg: "lg" }}
+                  >
+                    <NumberFormatter
+                      prefix="$ "
+                      value={t.price}
+                      thousandSeparator
+                      style={{ fontWeight: "bold" }}
+                    />
+                    <InquiryModalButton variant={"primary"} travelPackage={t}>
+                      Get a Quote
+                    </InquiryModalButton>
+                  </Flex>
+                </div>
+              </div>
+            </Flex>
+          </React.Fragment>
+        );
+      })}
+    </>
+  );
 };
 
 export default TravelPackage;
