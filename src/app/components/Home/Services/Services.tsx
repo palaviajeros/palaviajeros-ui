69398ded69100c340e88c8db6ecd2fa296994dbc
LeaHaelin <lea8haelin@gmail.com>
2024-07-10 14:53:25 +0200

    delete css and module.css files

--- src/app/components/Home/Services/Services.tsx
+++ src/app/components/Home/Services/Services.tsx
@@ -6,15 +6,15 @@ import {
   SimpleGrid,
   Container,
   rem,
-  Flex
+  Flex,
 } from "@mantine/core";
 import {
   IconGauge,
   IconUser,
   IconCookie,
-  IconProps
+  IconProps,
 } from "@tabler/icons-react";
-import classes from "@/app/components/Home/Services/Services.module.css";
+import classes from "@/app/components/Home/Services/Services.module.scss";
 
 interface ServicesCardContent {
   title: string;
@@ -27,20 +27,20 @@ const mockdata: ServicesCardContent[] = [
     title: "Extreme performance",
     description:
       "This dust is actually a powerful poison that will even make a pro wrestler sick, Regice cloaks itself with frigid air of -328 degrees Fahrenheit",
-    icon: IconGauge
+    icon: IconGauge,
   },
   {
     title: "Privacy focused",
     description:
       "People say it can run at the same speed as lightning striking, Its icy body is so cold, it will not melt even if it is immersed in magma",
-    icon: IconUser
+    icon: IconUser,
   },
   {
     title: "No third parties",
     description:
       "They’re popular, but they’re rare. Trainers who show them off recklessly may be targeted by thieves",
-    icon: IconCookie
-  }
+    icon: IconCookie,
+  },
 ];
 
 const Services: React.FC = () => {
