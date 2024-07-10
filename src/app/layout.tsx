7572ea8e820526580d14546e8e9f7a07955932ec
Sam Chu <samchupiano@gmail.com>
2024-07-10 06:42:53 +0200

    61 add notification banner soft launch (#66)
    * Add soft launch notification banner
    
    * Add placeholder to subscribe input component
    
    * Adjust homepage spacings
    
    * Add font weight
    
    * Adjust container spacings
    
    * Adjust classnames and spacings between components
    
    * Change classnames to camelCase

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,62 +1,68 @@
-import type {Metadata} from "next";
+import type { Metadata } from "next";
 import "@mantine/core/styles.css";
 import "@mantine/carousel/styles.css";
-import '@mantine/notifications/styles.css';
+import "@mantine/notifications/styles.css";
 
 import {
-    AppShell,
-    AppShellHeader,
-    AppShellMain,
-    ColorSchemeScript, Container,
-    createTheme,
-    MantineProvider
+  AppShell,
+  AppShellHeader,
+  AppShellMain,
+  ColorSchemeScript,
+  Container,
+  createTheme,
+  MantineProvider
 } from "@mantine/core";
 import Navbar from "./components/Navbar/Navbar";
 import Footer from "./components/Footer/Footer";
-import {Notifications} from "@mantine/notifications";
+import { Notifications } from "@mantine/notifications";
+import SoftLaunchBanner from "./components/SoftLaunchBanner/SoftLaunchBanner";
 
 export const metadata: Metadata = {
-    title: "Palaviajeros Travel Corp.",
-    description: "Your viajero buddy",
+  title: "Palaviajeros Travel Corp.",
+  description: "Your viajero buddy"
 };
 // palaviajeros logo color: #ff4c5c Todo: to add later
 const palaViajerosTheme = createTheme({
-    primaryColor: 'red',
-    breakpoints: {
-        xs: '30em',
-        sm: '48em',
-        md: '64em',
-        lg: '74em',
-        xl: '90em',
-    },
+  primaryColor: "red",
+  breakpoints: {
+    xs: "30em",
+    sm: "48em",
+    md: "64em",
+    lg: "74em",
+    xl: "90em"
+  }
 });
 
 export default function RootLayout({
-                                       children,
-                                   }: Readonly<{
-    children: React.ReactNode;
+  children
+}: Readonly<{
+  children: React.ReactNode;
 }>) {
-    return (
-        <html lang="en">
-        <head>
-            <ColorSchemeScript/>
-        </head>
-        <body>
+  return (
+    <html lang="en">
+      <head>
+        <ColorSchemeScript />
+      </head>
+      <body>
         <MantineProvider theme={palaViajerosTheme}>
-            <Notifications/>
-            <AppShell header={{height: {base: 36, sm: 48, lg: 60}}}>
-                <AppShellHeader>
-                    <Navbar/>
-                </AppShellHeader>
-                <AppShellMain>
-                    <Container size="lg" mt={"xl"}>
-                        {children}
-                    </Container>
-                    <Footer/>
-                </AppShellMain>
-            </AppShell>
+          <Notifications />
+          <AppShell header={{ height: { base: 48, lg: 60 } }}>
+            <AppShellHeader>
+              <Navbar />
+            </AppShellHeader>
+            <AppShellMain>
+              <Container
+                size="xl"
+                p={{ base: "50px 30px", sm: "50px", lg: "100px 70px" }}
+              >
+                {children}
+              </Container>
+              <Footer />
+            </AppShellMain>
+          </AppShell>
+          <SoftLaunchBanner />
         </MantineProvider>
-        </body>
-        </html>
-    );
+      </body>
+    </html>
+  );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,20 @@
 import Services from "./components/Home/Services/Services";
 import Header from "./components/Home/Header/Header";
 import Bestsellers from "./components/Home/Bestsellers/Bestsellers";
-import {Flex, Stack} from "@mantine/core";
+import { Flex } from "@mantine/core";
 import Subscribe from "@/app/components/Home/Subscribe/Subscribe";
 
 export default function Home() {
-    return (
-        <Flex gap={"xl"} direction={"column"} justify={"space-evenly"}>
-            <Header/>
-            <Bestsellers/>
-            <Services/>
-            <Subscribe/>
-        </Flex>
-    );
+  return (
+    <Flex
+      gap={{ base: "90px", sm: "140px", lg: "160px" }}
+      direction={"column"}
+      justify={"space-evenly"}
+    >
+      <Header />
+      <Bestsellers />
+      <Services />
+      <Subscribe />
+    </Flex>
+  );
 }
