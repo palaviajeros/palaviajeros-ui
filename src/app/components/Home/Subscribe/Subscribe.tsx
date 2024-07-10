cd38bc00aebd9352cad8dd9c518cc87fb9e2412e
Andrei de la Cruz <andrei.delacruz@gmail.com>
2024-07-11 01:33:27 +0200

    Fixed hydration issue caused by visibleFrom

--- src/app/components/Home/Subscribe/Subscribe.tsx
+++ src/app/components/Home/Subscribe/Subscribe.tsx
@@ -1,50 +1,52 @@
 import {
-  Button,
-  Container,
-  Flex,
-  Group,
-  Text,
-  TextInput,
-  Title
+    Button,
+    Container,
+    Flex,
+    Group,
+    Text,
+    TextInput,
+    Title
 } from "@mantine/core";
 import classes from "./Subscribe.module.scss";
 import React from "react";
 
 const Subscribe = () => {
-  return (
-    <Flex
-      direction={"column"}
-      justify={"center"}
-      className={classes.container}
-      gap={"xl"}
-    >
-      <Title ta={"center"} className={classes.title}>
-        Subscribe to get updates from your fellow Palaviajeros!
-      </Title>
-      <Group justify="center" mt="md">
-        <TextInput
-          size="xl"
-          radius="md"
-          inputSize="35"
-          visibleFrom={"xl"}
-          placeholder="Your email address"
-        ></TextInput>
-        <TextInput
-          size="md"
-          radius="md"
-          inputSize="35"
-          hiddenFrom={"xl"}
-          placeholder="Your email address"
-        ></TextInput>
-        <Button size="xl" radius="md" visibleFrom={"xl"}>
-          Contact Us
-        </Button>
-        <Button size="md" radius="md" hiddenFrom={"xl"}>
-          Contact Us
-        </Button>
-      </Group>
-    </Flex>
-  );
+    return (
+        <Flex
+            direction={"column"}
+            justify={"center"}
+            className={classes.container}
+            gap={"xl"}
+        >
+            <Title ta={"center"} className={classes.title}>
+                Subscribe to get updates from your fellow Palaviajeros!
+            </Title>
+            <Group justify="center" mt="md">
+                <TextInput
+                    size="xl"
+                    radius="md"
+                    inputSize="35"
+                    visibleFrom={"xl"}
+                    placeholder="Your email address"
+                    suppressHydrationWarning
+                />
+                <TextInput
+                    size="md"
+                    radius="md"
+                    inputSize="35"
+                    hiddenFrom={"xl"}
+                    placeholder="Your email address"
+                    suppressHydrationWarning
+                />
+                <Button size="xl" radius="md" visibleFrom={"xl"} suppressHydrationWarning>
+                    Contact Us
+                </Button>
+                <Button size="md" radius="md" hiddenFrom={"xl"} suppressHydrationWarning>
+                    Contact Us
+                </Button>
+            </Group>
+        </Flex>
+    );
 };
 
 export default Subscribe;
