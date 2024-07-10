9024d6373d6fa37f0fd5601cefa5530ac67c4ca2
Andrei de la Cruz <andrei.delacruz@gmail.com>
2024-07-11 00:10:02 +0200

    #47 Added inquiry functionality and added contact number validation

---
+++ src/app/components/Email/inquiry-email-template.tsx
@@ -0,0 +1,50 @@
+import * as React from 'react';
+import {TravelPackageDto} from "@/app/shared/models/travelPackageDto";
+
+export interface InquiryEmailTemplateProps {
+    name: string;
+    email: string;
+    contactNo: string;
+    travelDates: string;
+    noOfPax: string;
+    message: string;
+    inquiryDate: string;
+    travelPackage: TravelPackageDto;
+}
+
+export const InquiryEmailTemplate: React.FC<Readonly<InquiryEmailTemplateProps>> = (props: InquiryEmailTemplateProps) => (
+    <div>
+        <h1>Travel Inquiry for package: {props.travelPackage.name} (code : {props.travelPackage.code})</h1>
+        <br/>
+        <h2>Customer details:</h2>
+        <ul>
+            <li>
+                <b>Name</b>: {props.name}
+            </li>
+            <li>
+                <b>Email</b>: {props.email}
+            </li>
+            <li>
+                <b>Contact No</b>: {props.contactNo}
+            </li>
+        </ul>
+        <h2>Travel details:</h2>
+        <ul>
+            <li>
+                <b>Travel Package</b>: {props.travelPackage.name}
+            </li>
+            <li>
+                <b>Travel Dates</b>: {props.travelDates}
+            </li>
+            <li>
+                <b>No of People</b>: {props.noOfPax}
+            </li>
+            <li>
+                <b>Message</b>: {props.message}
+            </li>
+            <li>
+                <b>Time of inquiry</b>: {props.inquiryDate}
+            </li>
+        </ul>
+    </div>
+);
---
+++ src/app/lib/resend/send-inquiry.ts
@@ -0,0 +1,27 @@
+"use server";
+
+import { Resend } from "resend";
+import * as React from "react";
+import {
+  InquiryEmailTemplate,
+  InquiryEmailTemplateProps,
+} from "@/app/components/Email/inquiry-email-template";
+
+// Ask andrei to provide keys and how to set this up
+const resend = new Resend(process.env.RESEND_API_KEY);
+const emailSender = process.env.RESEND_EMAIL_SENDER;
+const emailRecipient = process.env.RESEND_INQUIRY_EMAIL_RECIPIENT;
+
+export async function sendInquiry(templateProps: InquiryEmailTemplateProps) {
+  const { data, error } = await resend.emails.send({
+    from: `Palaviajeros Inquiries <${emailSender}>`,
+    to: [`${emailRecipient}`],
+    subject: `Inquiry about ${templateProps.travelPackage.name} from ${templateProps.name} (${templateProps.email})`,
+    react: InquiryEmailTemplate(templateProps) as React.ReactElement,
+  });
+  if (error) {
+    console.error(error);
+    throw Error(error.message);
+  }
+  return data;
+}
