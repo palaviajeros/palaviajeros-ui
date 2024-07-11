eed85f8a9cd7d504b1be8c2d96621307defe1f6d
LeaHaelin <lea8haelin@gmail.com>
2024-07-11 18:36:04 +0200

    update the code with the recent version

--- src/app/shared/models/travelPackageDto.ts
+++ src/app/shared/models/travelPackageDto.ts
@@ -1,36 +1,36 @@
 export interface TravelCountryPackages {
-    countryCode: string,
-    countryName: string,
-    description: string,
-    packages: TravelPackageDto[],
+  countryCode: string;
+  countryName: string;
+  description: string;
+  packages: TravelPackageDto[];
 }
 
 export interface TravelPackageDto {
-    code: string,
-    name: string,
-    description: string[],
-    inclusions: Services[],
-    exclusions: Services[],
-    travelDates: DateRange[],
-    itinerary: DayPlan[],
-    imageUrls: string[],
-    price: number,
+  code: string;
+  name: string;
+  description: string[];
+  inclusions: Services[];
+  exclusions: Services[];
+  travelDates: DateRange[];
+  itinerary: DayPlan[];
+  imageUrls: string[];
+  price: number;
 }
 
 export interface DayPlan {
-    dayNo: number,
-    activities: string[]
+  dayNo: number;
+  activities: string[];
 }
 
 export interface DateRange {
-    startDate: Date,
-    endDate: Date,
+  startDate: Date;
+  endDate: Date;
 }
 
 export enum Services {
-    FLIGHT = 'Flight',
-    ACCOMMODATION = 'Accommodation',
-    MEALS = 'Meals',
-    TRANSFERS = 'Transfers',
-    TOURS = 'Tours',
+  FLIGHT = "Flight",
+  ACCOMMODATION = "Accommodation",
+  MEALS = "Meals",
+  TRANSFERS = "Transfers",
+  TOURS = "Tours",
 }
