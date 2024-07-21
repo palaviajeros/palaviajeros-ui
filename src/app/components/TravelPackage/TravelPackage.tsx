e7a26a90e9e98d0b93b95fae057bdfc23d9c5b5e
Andrei de la Cruz <andrei.delacruz@gmail.com>
2024-07-21 22:30:01 +0200

    Chore travel packages fix image size issue (#87)
    * Fixe unused import
    
    * Fixed sizing of package images
    
    * Merge with main
    
    * Ran npx @next/codemod next-image-to-legacy-image  to update references

--- src/app/components/TravelPackage/TravelPackage.tsx
+++ src/app/components/TravelPackage/TravelPackage.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Image from "next/image";
+import NextImage from "next/legacy/image";
 import { TravelCountryPackages } from "@/app/shared/models/travelPackageDto";
 import { Carousel } from "@mantine/carousel";
 import {
@@ -10,6 +10,7 @@ import {
   Title,
   Text,
   Tooltip,
+  Image,
 } from "@mantine/core";
 import {
   IconCheck,
@@ -48,6 +49,7 @@ const TravelPackage = ({ travelPackage }: TravelPackageProps) => {
                   align="center"
                   loop
                   height="100%"
+                  mb={10}
                   nextControlIcon={
                     <IconCircleArrowRightFilled
                       style={{
@@ -72,11 +74,14 @@ const TravelPackage = ({ travelPackage }: TravelPackageProps) => {
                       <Carousel.Slide key={index}>
                         <Image
                           className="package-photo"
+                          component={NextImage}
+                          priority={index == 0}
                           layout="responsive"
                           width={14}
                           height={9}
                           src={img}
                           alt="image"
+                          radius={"md"}
                         />
                       </Carousel.Slide>
                     );
