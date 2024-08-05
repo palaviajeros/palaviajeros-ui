922d6f9e1f14177cf10001f1e4932bc18ac96d9d
LeaHaelin <lea8haelin@gmail.com>
2024-08-05 15:29:40 +0200

    restyle footer

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,7 +10,7 @@ import {
   ColorSchemeScript,
   Container,
   createTheme,
-  MantineProvider
+  MantineProvider,
 } from "@mantine/core";
 import Navbar from "./components/Navbar/Navbar";
 import Footer from "./components/Footer/Footer";
@@ -21,7 +21,7 @@ export const metadata: Metadata = {
   title: "Palaviajeros Travel Corp.",
   description: "Your viajero travel buddy",
   keywords:
-    "travel, travel agency, philippines, leisure, packages, flights, itineraries"
+    "travel, travel agency, philippines, leisure, packages, flights, itineraries",
 };
 // palaviajeros logo color: #ff4c5c Todo: to add later
 const palaViajerosTheme = createTheme({
@@ -31,12 +31,12 @@ const palaViajerosTheme = createTheme({
     sm: "48em",
     md: "64em",
     lg: "74em",
-    xl: "90em"
-  }
+    xl: "90em",
+  },
 });
 
 export default function RootLayout({
-  children
+  children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
