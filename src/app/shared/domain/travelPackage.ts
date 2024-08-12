ab5358af4068689787032e9891ed48cd16131cd2
Sam Chu <samchupiano@gmail.com>
2024-08-12 13:38:47 +0200

    Add price to package slider card and fix z-index on new services on mobile (#125)

--- src/app/shared/domain/travelPackage.ts
+++ src/app/shared/domain/travelPackage.ts
@@ -1,26 +1,27 @@
-import {Services} from "@/app/shared/domain/services";
+import { Services } from "@/app/shared/domain/services";
 
 export interface TravelPackage {
-    code: string,
-    name: string,
-    isFlexible?: boolean,
-    description: string[],
-    inclusions: Services[],
-    exclusions: Services[],
-    days: number,
-    travelDates: Date[],
-    itinerary: DayPlan[],
-    imageUrls: string[],
-    price: number,
-    tags?: string[],
+  code: string;
+  name: string;
+  isFlexible?: boolean;
+  description: string[];
+  inclusions: Services[];
+  exclusions: Services[];
+  days: number;
+  travelDates: Date[];
+  itinerary: DayPlan[];
+  imageUrls: string[];
+  price: number;
+  salePrice?: number;
+  tags?: string[];
 }
 
 export interface DayPlan {
-    dayNo: number;
-    activities: string[];
+  dayNo: number;
+  activities: string[];
 }
 
 export interface DateRange {
-    startDate: Date;
-    endDate: Date;
+  startDate: Date;
+  endDate: Date;
 }
