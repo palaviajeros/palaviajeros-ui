1b4fcdbab6fd8a9ca03ac88209067a8a0967d3be
Sam Chu <samchupiano@gmail.com>
2024-08-22 12:06:56 +0200

    Update packages slider to accept more packages

--- src/app/components/Home/PackageSlider/PackageSlider.tsx
+++ src/app/components/Home/PackageSlider/PackageSlider.tsx
@@ -1,28 +1,23 @@
 import PackageSliderCard from "./PackageSliderCard";
-import {Flex, Title} from "@mantine/core";
+import { Flex, Title } from "@mantine/core";
 import classes from "@/app/components/Home/Services/Services.module.scss";
-import {findPackagesPerCountry} from "@/app/actions/travelPackagesLoader";
-import {TravelPackage} from "@/app/shared/domain/travelPackage";
-
-const NO_OF_PACKAGES: number = 3;
-const BEST_SELLER_TAG = "best-seller";
+import { findPackagesPerCountry } from "@/app/actions/travelPackagesLoader";
+import { TravelPackage } from "@/app/shared/domain/travelPackage";
 
 interface CardCarouselProps {
-    noOfCards: number;
-    tagFilter: string;
-    title: string;
+  noOfRenderedCards: number;
+  tagFilter: string;
+  title: string;
 }
 
-export default async function PackageSlider({noOfCards, tagFilter, title}: CardCarouselProps) {
-    const packages: TravelPackage[] = await findPackagesPerCountry(
-        (p, i) => p.tags?.includes(tagFilter) || false);
-
-    return (
-        <Flex direction="column">
-            <Title ta={"center"} order={2} className={classes.title}>
-                {title}
-            </Title>
-            <PackageSliderCard key="best-sellers" travelPackages={packages.slice(0, noOfCards)}/>
-        </Flex>
-    );
+export default async function PackageSlider({ noOfRenderedCards, tagFilter, title }: CardCarouselProps) {
+  const packages: TravelPackage[] = await findPackagesPerCountry((p, i) => p.tags?.includes(tagFilter) || false);
+  return (
+    <Flex direction="column">
+      <Title ta={"center"} order={2} className={classes.title}>
+        {title}
+      </Title>
+      <PackageSliderCard key="best-sellers" travelPackages={packages} noOfRenderedCards={noOfRenderedCards} />
+    </Flex>
+  );
 }
