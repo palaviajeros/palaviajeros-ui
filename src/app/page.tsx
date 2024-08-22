430964b9fabe07331b9ce74559c886c2b1cfedf7
Sam Chu <samchupiano@gmail.com>
2024-08-22 12:20:12 +0200

    Update packages slider to accept more packages (#134)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,26 +1,22 @@
 import Services from "./components/Home/Services/Services";
 import Header from "./components/Home/Header/Header";
-import {Flex} from "@mantine/core";
+import { Flex } from "@mantine/core";
 import Subscribe from "@/app/components/Home/Subscribe/Subscribe";
-import {Metadata} from "next";
+import { Metadata } from "next";
 import PackageSlider from "@/app/components/Home/PackageSlider/PackageSlider";
 
 export const metadata: Metadata = {
-    title: "Home | Palaviajeros Travel Corp",
+  title: "Home | Palaviajeros Travel Corp"
 };
 export default function Home() {
-    return (
-        <Flex
-            gap={{base: "90px", sm: "140px", lg: "160px"}}
-            direction={"column"}
-            justify={"space-evenly"}
-        >
-            <Header/>
-            <PackageSlider noOfCards={3} title="Top Destinations" tagFilter="best-seller"/>
-            <PackageSlider noOfCards={3} title="Hot Deals" tagFilter="sale"/>
-            <PackageSlider noOfCards={3} title="Featured Packages" tagFilter="featured"/>
-            <Services/>
-            <Subscribe/>
-        </Flex>
-    );
+  return (
+    <Flex gap={{ base: "90px", sm: "140px", lg: "160px" }} direction={"column"} justify={"space-evenly"}>
+      <Header />
+      <PackageSlider noOfRenderedCards={3} title="Top Destinations" tagFilter="best-seller" />
+      <PackageSlider noOfRenderedCards={3} title="Hot Deals" tagFilter="sale" />
+      <PackageSlider noOfRenderedCards={3} title="Featured Packages" tagFilter="featured" />
+      <Services />
+      <Subscribe />
+    </Flex>
+  );
 }
