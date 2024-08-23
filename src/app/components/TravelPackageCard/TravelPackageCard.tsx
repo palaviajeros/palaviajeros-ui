efe6f8ec5289529f0e82a17b15a9e0b5c5e05454
Andrei de la Cruz <andrei.delacruz@gmail.com>
2024-08-23 02:50:03 +0200

    Refactor and rename package carousel

---
+++ src/app/components/TravelPackageCard/TravelPackageCard.tsx
@@ -0,0 +1,66 @@
+import { TravelPackage } from "@/app/shared/domain/travelPackage";
+import { Badge, Button, Card, Flex, Image, Text } from "@mantine/core";
+import NextImage from "next/image";
+import InquiryModal from "@/app/components/InquiryModal/InquiryModalButton";
+
+interface TravelPackageCardProps {
+  travelPackage: TravelPackage;
+}
+
+export default function TravelPackageCard({
+  travelPackage,
+}: TravelPackageCardProps) {
+  return (
+    <Card shadow="sm" padding="lg" radius="md" withBorder h="100%">
+      <Card.Section>
+        <Image
+          component={NextImage}
+          src={travelPackage.imageUrls[0]}
+          alt={travelPackage.code}
+          height={300}
+          width={400}
+        />
+      </Card.Section>
+
+      <Text fw={500} mt="md">
+        {travelPackage.name}
+      </Text>
+      <Badge variant="light" leftSection="📅" mt="md" mb="md">
+        {travelPackage.days} days trip
+      </Badge>
+      <Text size="sm" c="dimmed">
+        {travelPackage.description}
+      </Text>
+      <Flex align="flex-end" flex={1}>
+        <Text
+          fw="bold"
+          fz="sm"
+          td={travelPackage.salePrice ? "line-through" : "none"}
+        >
+          ₱{travelPackage.price}
+        </Text>
+        <Text
+          fw="bold"
+          ml="xs"
+          fz="sm"
+          display={travelPackage.salePrice ? "block" : "none"}
+          c="var(--mantine-color-red-8)"
+        >
+          ₱{travelPackage.salePrice}
+        </Text>
+        <InquiryModal travelPackage={travelPackage} variant={"default"}>
+          <Button
+            variant="default"
+            mt="md"
+            radius="lg"
+            size="xs"
+            maw={150}
+            ml="auto"
+          >
+            Request a Quote
+          </Button>
+        </InquiryModal>
+      </Flex>
+    </Card>
+  );
+}
