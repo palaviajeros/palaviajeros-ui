5f78fb8710c30841a437ce0e45a7f0dae3b060c2
LeaHaelin <lea8haelin@gmail.com>
2024-08-27 17:07:12 +0200

    replace previous to packageCarousel component

--- src/app/packages/countries/[countryCode]/page.tsx
+++ src/app/packages/countries/[countryCode]/page.tsx
@@ -1,23 +1,27 @@
 import React from "react";
 import { findCountryPackage } from "@/app/actions/travelPackagesLoader";
 import { TravelPackage as TravelPackageModel } from "@/app/shared/domain/travelPackage";
-import PackageSliderCard from "@/app/components/Home/PackageSlider/PackageSliderCard";
 import { Flex, Title } from "@mantine/core";
+import PackageCarousel from "@/app/components/Home/PackageCarousel/PackageCarousel";
 
 interface CountryLandingPageProps {
   countryCode: string;
 }
 
 const CountryPackage = async ({ params }: { params: CountryLandingPageProps }) => {
-  const travelCountryPackage = await findCountryPackage((cp) => cp.countryCode == params.countryCode);
+  const travelCountryPackage = await findCountryPackage(cp => cp.countryCode == params.countryCode);
   const travelPackages: TravelPackageModel[] = travelCountryPackage?.packages || [];
   if (!travelCountryPackage) {
     return <>no packages found</>;
   }
+
   return (
     <>
-      <Flex>
-        <PackageSliderCard travelPackages={travelPackages} noOfRenderedCards={travelPackages.length} />
+      <Flex direction="column">
+        <Title order={2} ta="center">
+          All Packages
+        </Title>
+        <PackageCarousel travelPackages={travelPackages} noOfRenderedCards={travelPackages.length} />
       </Flex>
     </>
   );
