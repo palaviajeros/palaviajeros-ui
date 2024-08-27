64093d8f5599b6faeecea13ec734d136ee11e836
LeaHaelin <lea8haelin@gmail.com>
2024-08-27 16:46:06 +0200

    display carousel

--- src/app/packages/countries/[countryCode]/page.tsx
+++ src/app/packages/countries/[countryCode]/page.tsx
@@ -1,23 +1,26 @@
 import React from "react";
 import { findCountryPackage } from "@/app/actions/travelPackagesLoader";
-import TravelPackage from "@/app/components/TravelPackage/TravelPackage";
+import { TravelPackage as TravelPackageModel } from "@/app/shared/domain/travelPackage";
+import PackageSliderCard from "@/app/components/Home/PackageSlider/PackageSliderCard";
+import { Flex, Title } from "@mantine/core";
 
 interface CountryLandingPageProps {
   countryCode: string;
 }
 
-const CountryPackage = async ({
-  params,
-}: {
-  params: CountryLandingPageProps;
-}) => {
-  const travelCountryPackage = await findCountryPackage(
-    (cp) => cp.countryCode == params.countryCode
-  );
+const CountryPackage = async ({ params }: { params: CountryLandingPageProps }) => {
+  const travelCountryPackage = await findCountryPackage((cp) => cp.countryCode == params.countryCode);
+  const travelPackages: TravelPackageModel[] = travelCountryPackage?.packages || [];
   if (!travelCountryPackage) {
     return <>no packages found</>;
   }
-  return <TravelPackage travelPackage={travelCountryPackage} />;
+  return (
+    <>
+      <Flex>
+        <PackageSliderCard travelPackages={travelPackages} noOfRenderedCards={travelPackages.length} />
+      </Flex>
+    </>
+  );
 };
 
 export default CountryPackage;
