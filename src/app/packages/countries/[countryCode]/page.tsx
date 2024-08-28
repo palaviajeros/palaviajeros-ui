9d774cf3d909a594af251760f0a121c11f2cb9eb
Lea Haelin Kim <87936540+LeaHaelin@users.noreply.github.com>
2024-08-28 17:11:42 +0200

    130 create travelpackage id skeleton landing page (#142)
    * display carousel
    
    * replace previous to packageCarousel component
    
    * refactor card display
    
    * remove unused mantine item

--- src/app/packages/countries/[countryCode]/page.tsx
+++ src/app/packages/countries/[countryCode]/page.tsx
@@ -1,23 +1,38 @@
 import React from "react";
 import { findCountryPackage } from "@/app/actions/travelPackagesLoader";
-import TravelPackage from "@/app/components/TravelPackage/TravelPackage";
+import { TravelPackage as TravelPackageModel } from "@/app/shared/domain/travelPackage";
+import { Flex, Title } from "@mantine/core";
+import TravelPackageCard from "@/app/components/TravelPackageCard/TravelPackageCard";
 
 interface CountryLandingPageProps {
   countryCode: string;
 }
 
-const CountryPackage = async ({
-  params,
-}: {
-  params: CountryLandingPageProps;
-}) => {
-  const travelCountryPackage = await findCountryPackage(
-    (cp) => cp.countryCode == params.countryCode
-  );
+const CountryPackage = async ({ params }: { params: CountryLandingPageProps }) => {
+  const travelCountryPackage = await findCountryPackage(cp => cp.countryCode == params.countryCode);
+  const travelPackages: TravelPackageModel[] = travelCountryPackage?.packages || [];
   if (!travelCountryPackage) {
     return <>no packages found</>;
   }
-  return <TravelPackage travelPackage={travelCountryPackage} />;
+  return (
+    <>
+      <Flex direction="column" gap="lg">
+        <Title order={2} ta="center">
+          {travelCountryPackage.countryName}
+        </Title>
+
+        <Flex direction={{ base: "column", md: "row" }} gap="md">
+          {travelPackages.map((tp: TravelPackageModel) => {
+            return (
+              <div key={tp.code} style={{ flex: 1 }}>
+                <TravelPackageCard travelPackage={tp} />
+              </div>
+            );
+          })}
+        </Flex>
+      </Flex>
+    </>
+  );
 };
 
 export default CountryPackage;
