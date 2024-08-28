86a92812546443ac11f308da81826a3dc9d05804
LeaHaelin <lea8haelin@gmail.com>
2024-08-28 16:08:02 +0200

    refactor card display

--- src/app/packages/countries/[countryCode]/page.tsx
+++ src/app/packages/countries/[countryCode]/page.tsx
@@ -1,8 +1,8 @@
 import React from "react";
 import { findCountryPackage } from "@/app/actions/travelPackagesLoader";
 import { TravelPackage as TravelPackageModel } from "@/app/shared/domain/travelPackage";
-import { Flex, Title } from "@mantine/core";
-import PackageCarousel from "@/app/components/Home/PackageCarousel/PackageCarousel";
+import { Flex, Title, Card } from "@mantine/core";
+import TravelPackageCard from "@/app/components/TravelPackageCard/TravelPackageCard";
 
 interface CountryLandingPageProps {
   countryCode: string;
@@ -14,14 +14,22 @@ const CountryPackage = async ({ params }: { params: CountryLandingPageProps }) =
   if (!travelCountryPackage) {
     return <>no packages found</>;
   }
-
   return (
     <>
-      <Flex direction="column">
+      <Flex direction="column" gap="lg">
         <Title order={2} ta="center">
-          All Packages
+          {travelCountryPackage.countryName}
         </Title>
-        <PackageCarousel travelPackages={travelPackages} noOfRenderedCards={travelPackages.length} />
+
+        <Flex direction={{ base: "column", md: "row" }} gap="md">
+          {travelPackages.map((tp: TravelPackageModel) => {
+            return (
+              <div key={tp.code} style={{ flex: 1 }}>
+                <TravelPackageCard travelPackage={tp} />
+              </div>
+            );
+          })}
+        </Flex>
       </Flex>
     </>
   );
