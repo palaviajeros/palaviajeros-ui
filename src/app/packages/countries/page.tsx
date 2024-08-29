9d689fe072e8ed7df82d34e1fd71af4d3c9bee0d
Sam Chu <samchupiano@gmail.com>
2024-08-29 12:29:16 +0200

    test-3

--- src/app/packages/countries/page.tsx
+++ src/app/packages/countries/page.tsx
@@ -1,9 +1,11 @@
 import { getCountryTravelPackages } from "@/app/actions/travelPackagesLoader";
 import Link from "next/link";
 import { Flex, BackgroundImage, Box, Title, Text, Space } from "@mantine/core";
+import { TravelCountryPackage } from "@/app/shared/domain/countryPackage";
 
 export default async function Countries() {
-  const countries = await getCountryTravelPackages();
+  const countries: TravelCountryPackage[] = await getCountryTravelPackages();
+
   return (
     <>
       <Title order={1} ta="center">
@@ -11,7 +13,7 @@ export default async function Countries() {
       </Title>
       <Space h="xl" />
       <Flex justify="space-evenly" wrap="wrap" gap="20px">
-        {countries.map((country) => (
+        {countries.map(country => (
           <Link
             key={country.countryCode}
             href={{
@@ -20,19 +22,11 @@ export default async function Countries() {
             style={{ textDecoration: "none" }}
           >
             <Box>
-              <BackgroundImage
-                w={400}
-                h={240}
-                src={country.packages[0].imageUrls[0]}
-                radius="md"
-                p={20}
-              >
+              <BackgroundImage w={400} h={240} src={country.packages[0].imageUrls[0]} radius="md" p={20}>
                 <Title order={3} c="var(--mantine-color-white)">
                   {country.countryName}
                 </Title>
-                <Text c="var(--mantine-color-white)">
-                  {country.description}
-                </Text>
+                <Text c="var(--mantine-color-white)">{country.description}</Text>
               </BackgroundImage>
             </Box>
           </Link>
