7473559fdc68de2cc7ab063cae0dfe644c3b93c1
LeaHaelin <lea8haelin@gmail.com>
2024-09-04 13:47:59 +0200

    update content and refactor styles

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,7 +1,5 @@
 import React from "react";
 import { Title, Text, Image, Space, Flex, Box } from "@mantine/core";
-// import { title } from "process";
-import classes from "@/app/about/about.module.scss";
 import { Metadata } from "next";
 
 export const metadata: Metadata = {
@@ -10,40 +8,23 @@ export const metadata: Metadata = {
 
 interface ContentsProps {
   title: string;
-  text: string;
+  text: React.ReactNode;
   imageUrl: string;
   reverse: boolean; //to reverse the flex direction
 }
 
-const ContentBlock: React.FC<ContentsProps> = ({
-  title,
-  text,
-  imageUrl,
-  reverse,
-}) => {
+const ContentBlock: React.FC<ContentsProps> = ({ title, text, imageUrl, reverse }) => {
   return (
-    <Flex
-      direction={reverse ? "row-reverse" : "row"}
-      wrap={{ base: "wrap", lg: "unset" }}
-      gap={50}
-      justify="space-evenly"
-      miw={300}
-    >
+    <Flex direction={{ base: "column", md: reverse ? "row-reverse" : "row" }} wrap={{ base: "wrap", lg: "unset" }} gap={20}>
       <Flex flex={1} direction="column" wrap="wrap">
-        <Title order={2} size="h1" mb={10} ta={{ base: "center", lg: "start" }}>
+        <Title order={2} size="h1" mb={10} ta={{ base: "center", md: "start" }}>
           {title}
         </Title>
-        <Text size="md">{text}</Text>
+        <Text size="md" ta={{ base: "center", md: "start" }}>
+          {text}
+        </Text>
       </Flex>
-      <Image
-        flex={1}
-        src={imageUrl}
-        alt={title}
-        w={{ base: "300", sm: "auto" }}
-        fit="contain"
-        h="100%"
-        radius="md"
-      />
+      <Image flex={1} src={imageUrl} alt={title} w={"100%"} fit="contain" h="100%" radius="md" />
     </Flex>
   );
 };
@@ -51,23 +32,51 @@ const ContentBlock: React.FC<ContentsProps> = ({
 const About: React.FC = () => {
   const contents = [
     {
-      title: "Sample",
-      text: "This is a sample text that describes the content of the image below. This is a sample text that describes the content of the image below.",
+      title: "Travel brings us closer",
+      text: (
+        <>
+          Palaviajeros started in 2024 but founders (and friends) have been traveling for almost a decade. Palaviajeros is a compilation of
+          travel expertise and experiences from backpacking to luxury resorts, from budget to full-service airlines, from self-guided to
+          personalized tours. Our mission is make travel as comfortable as possible to all kinds of travelers.
+        </>
+      ),
       imageUrl: "/samples/sample1.jpg",
     },
     {
-      title: "Sample",
-      text: "This is a sample text that describes the content of the image below.This is a sample text that describes the content of the image below. This is a sample text that describes the content of the image below.",
+      title: "Experience is key",
+      text: (
+        <>
+          All tours are curated by our master traveler, Victor. Victor, half-American half-Filipino, currently resides in Barcelona but has
+          lives in different parts of the worlds and has been traveling for over 20 years looking for best destinations and tours around the
+          world to date. He has visited 57 countries, still considers himself a novice, but specializes in lavish tours.
+          <Space h="md" />
+          Booking at Palaviajeros, you’ll get direct contact to Victor and his myriad experience in traveling the world.
+        </>
+      ),
       imageUrl: "/samples/sample2.jpg",
     },
     {
-      title: "Sample",
-      text: "This is a sample text that describes the content of the image below. This is a sample text that describes the content of the image below. This is a sample text that describes the content of the image below.",
+      title: "Experiences, not checkbox",
+      text: (
+        <>
+          These days, travel is not just about ticking a checkbox…experiences count more. Sure, everyone’s been to Singapore, New York or
+          even London. But do you want to hike the mountains of Machu Picchu, each paella by the beaches of Costa Brava, experience the
+          exotic nightlife of Bangkok or witness lion hunting a gazelle in the planes of Africa? With Palaviajeros, we’ll help you plan the
+          best itinerary based on your likes, personality and of course budget.
+        </>
+      ),
       imageUrl: "/samples/sample3.jpg",
     },
     {
-      title: "Sample",
-      text: "This is a sample text that describes the content of the image below. This is a sample text that describes the content of the image below. This is a sample text that describes the content of the image below.This is a sample text that describes the content of the image below. This is a sample text that describes the content of the image below.",
+      title: "Disconnect to connect",
+      text: (
+        <>
+          In the age of tiktok, we are so consumed by what we see on social media where the measure of us enjoying life is by seeing how
+          much time we’re spending offline. Yet we still need to post online to feel connected.
+          <Space h="md" /> Experience life with Palaviajeros! Check our our Instagram, facebook and tiktok to see our latest offer. Check
+          new destinations and promotional offers on our social media accounts."
+        </>
+      ),
       imageUrl: "/samples/sample3.jpg",
     },
   ];
@@ -78,13 +87,7 @@ const About: React.FC = () => {
       </Title>
       <Flex direction="column" gap={{ base: "50", lg: "100" }}>
         {contents.map((content, index) => (
-          <ContentBlock
-            key={index}
-            title={content.title}
-            text={content.text}
-            imageUrl={content.imageUrl}
-            reverse={index % 2 === 1}
-          />
+          <ContentBlock key={index} title={content.title} text={content.text} imageUrl={content.imageUrl} reverse={index % 2 === 0} />
         ))}
       </Flex>
     </>
