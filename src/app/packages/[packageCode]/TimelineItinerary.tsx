292346c3277a79e7511f9ca634a69fa8d36e2574
Sam Chu <samchupiano@gmail.com>
2024-09-30 15:54:51 +0200

    make itinerary timeline color consistent, also change the logo inside the box (#188)

--- src/app/packages/[packageCode]/TimelineItinerary.tsx
+++ src/app/packages/[packageCode]/TimelineItinerary.tsx
@@ -2,17 +2,17 @@
 
 import { Text, Timeline } from "@mantine/core";
 import { TravelPackage } from "@/app/shared/domain/travelPackage";
-import { IconGitBranch } from "@tabler/icons-react";
+import { IconMapSearch } from "@tabler/icons-react";
 interface TimelineItineraryProps {
   travelPackage: TravelPackage;
 }
 
 function TimelineItinerary({ travelPackage }: TimelineItineraryProps) {
   return (
-    <Timeline active={2} bulletSize={24} lineWidth={2}>
+    <Timeline active={travelPackage.days} bulletSize={24} lineWidth={2}>
       {travelPackage.itinerary.map(itinerary => {
         return (
-          <Timeline.Item key={itinerary.dayNo} bullet={<IconGitBranch size={12} />} title={`Day ${itinerary.dayNo}: ${itinerary.title}`}>
+          <Timeline.Item key={itinerary.dayNo} bullet={<IconMapSearch size={14} />} title={`Day ${itinerary.dayNo}: ${itinerary.title}`}>
             {itinerary.activities.map((activity, index) => (
               <Text c="dimmed" size="sm" key={index}>
                 • {activity}
